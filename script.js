ece7df0cddbd23f0fd40d1efd3cff46c36e3561f
Lukas Martínez <lukasmartinezc@gmail.com>
2025-02-10 23:30:21 -0300

    Jankenpon finished

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const input = document.getElementById('name')
-let username = document.getElementById('username')
+const username = document.getElementById('username')
 const label = document.querySelector('label')
 
 const rockUser = document.getElementById('rock-user')
@@ -9,8 +9,6 @@ const scissorsUser = document.getElementById('scissors-user')
 const rockComputer = document.getElementById('rock-computer')
 const paperComputer = document.getElementById('paper-computer')
 const scissorsComputer = document.getElementById('scissors-computer')
-
-let optionPc = ['rock', 'paper', 'scissors']
 
 input.addEventListener('keydown', (e) => {
     if(e.key === 'Enter') {
@@ -26,19 +24,6 @@ function getRandomElement(arr) {
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
-function notSelected(option) {
-    if(option === 'rock') {
-        paperComputer.style.backgroundColor = '#cacbd0'
-        scissorsComputer.style.backgroundColor = '#cacbd0'
-    } else if(option === 'paper') {
-        rockComputer.style.backgroundColor = '#cacbd0'
-        scissorsComputer.style.backgroundColor = '#cacbd0'
-    } else {
-        rockComputer.style.backgroundColor = '#cacbd0'
-        paperComputer.style.backgroundColor = '#cacbd0'
-    }
-}
-
 function reseatOption() {
     rockUser.style.backgroundColor = '#ff9a68'
     paperUser.style.backgroundColor = '#019ee6'
@@ -48,7 +33,16 @@ function reseatOption() {
     scissorsComputer.style.backgroundColor = '#fbca03'
 }
 
-rockUser.addEventListener('click', () => {
+function notSelected(option) {
+    [rockComputer, paperComputer, scissorsComputer].forEach(btn => {
+        btn.style.background = option === btn.dataset.value ? '' :  '#cacbd0'
+    })
+}
+
+function playGame(optionUser) {
+    const { selected, notSelectedOne, notSelectedTwo } = optionUser
+    let optionPc = ['rock', 'paper', 'scissors']
+
     if(!username.textContent) {
         Swal.fire({
             icon: "error",
@@ -56,27 +50,26 @@ rockUser.addEventListener('click', () => {
             text: "Enter your name to play",
         });  
     } else {
-        paperUser.style.backgroundColor = '#cacbd0'
-        scissorsUser.style.backgroundColor = '#cacbd0'
-    
-        let optionUser = rockUser.dataset.value
-        let randomElement = getRandomElement(optionPc)
-    
+        notSelectedOne.style.backgroundColor = '#cacbd0'
+        notSelectedTwo.style.backgroundColor = '#cacbd0'
+
+        const randomElement = getRandomElement(optionPc)
+
         notSelected(randomElement)
-        
-        if(randomElement === optionUser) {
+
+        if(randomElement === selected) {
             Swal.fire({
                 title: "Draw",
-                text: "It repeats",
-                icon: "question"
+                icon: "question",
+                position: "top-end"
             }).then(() => {
                 reseatOption()
             })
-        } else if(randomElement === 'paper') {
+        } else if(randomElement === notSelectedOne.dataset.value) {
             Swal.fire({
                 icon: "error",
                 title: "You lost",
-                text: "Good luck next time",
+                position: "top-end"
             }).then(() => {
                 reseatOption()
             })
@@ -84,105 +77,35 @@ rockUser.addEventListener('click', () => {
             Swal.fire({
                 title: "You won!",
                 icon: "success",
-                draggable: true
+                draggable: true,
+                position: "top-end"
             }).then(() => {
                 reseatOption()
             })
         }
     }
+}
 
-})
+const optionRock = {
+    selected: rockUser.dataset.value,
+    notSelectedOne: paperUser,
+    notSelectedTwo: scissorsUser
+}
 
-paperUser.addEventListener('click', () => {
-    if(!username.textContent) {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Enter your name to play",
-        });  
-    } else {
-        rockUser.style.backgroundColor = '#cacbd0'
-        scissorsUser.style.backgroundColor = '#cacbd0'
-    
-        let optionUser = paperUser.dataset.value
-        let randomElement = getRandomElement(optionPc)
-    
-        notSelected(randomElement)
-        
-        if(randomElement === optionUser) {
-            Swal.fire({
-                title: "Draw",
-                text: "It repeats",
-                icon: "question"
-            }).then(() => {
-                reseatOption()
-            })
-        } else if(randomElement === 'scissors') {
-            Swal.fire({
-                icon: "error",
-                title: "You lost",
-                text: "Good luck next time",
-            }).then(() => {
-                reseatOption()
-            })
-        } else {
-            Swal.fire({
-                title: "You won!",
-                icon: "success",
-                draggable: true
-            }).then(() => {
-                reseatOption()
-            })
-        }
-    }
-})
+rockUser.addEventListener('click', () => playGame(optionRock))
 
-scissorsUser.addEventListener('click', () => {
-    if(!username.textContent) {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Enter your name to play",
-        });  
-    } else {
-        paperUser.style.backgroundColor = '#cacbd0'
-        rockUser.style.backgroundColor = '#cacbd0'
-    
-        let optionUser = scissorsUser.dataset.value
-        let randomElement = getRandomElement(optionPc)
-    
-        notSelected(randomElement)
-        
-        if(randomElement === optionUser) {
-            Swal.fire({
-                title: "Draw",
-                text: "It repeats",
-                icon: "question"
-            }).then(() => {
-                reseatOption()
-            })
-        } else if(randomElement === 'rock') {
-            Swal.fire({
-                icon: "error",
-                title: "You lost",
-                text: "Good luck next time",
-            }).then(() => {
-                reseatOption()
-            })
-        } else {
-            Swal.fire({
-                title: "You won!",
-                icon: "success",
-                draggable: true
-            }).then(() => {
-                reseatOption()
-            })
-        }
-    }
+const optionPaper = {
+    selected: paperUser.dataset.value,
+    notSelectedOne: scissorsUser,
+    notSelectedTwo: rockUser
+}
 
-})
+paperUser.addEventListener('click', () => playGame(optionPaper))
 
- 
+const optionScissors = {
+    selected: scissorsUser.dataset.value,
+    notSelectedOne: rockUser,
+    notSelectedTwo: paperUser
+}
 
-
-
+scissorsUser.addEventListener('click', () => playGame(optionScissors))
